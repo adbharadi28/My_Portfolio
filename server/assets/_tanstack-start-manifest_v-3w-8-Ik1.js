bb40197ff3808c66adcea1a88bb980c1092fbcad
Arnav Bharadi <205428183+adbharadi28@users.noreply.github.com>
2026-05-16 18:02:33 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-3w-8-Ik1.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Arnav Bharadi Portfolio/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/My_Portfolio/assets/index-Djl8LZ7T.js"] }, "/": { filePath: "D:/Arnav Bharadi Portfolio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/My_Portfolio/assets/index-Dh6pf-pG.js"] } }, clientEntry: "/My_Portfolio/assets/index-Djl8LZ7T.js" });
+export {
+  tsrStartManifest
+};
